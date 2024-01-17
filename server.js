d73e993519e967c98258eddba75a428f730de6b8
Dumper003 <108779952+Dumper003@users.noreply.github.com>
2024-01-17 15:31:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: ['https://kolva.online', 'https://dumper-true.xyz', 'https://fulldeportes.live'],
+  originWhitelist: ['https://kolva.online', 'https://dumper-true.com', 'https://fulldeportes.live'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
